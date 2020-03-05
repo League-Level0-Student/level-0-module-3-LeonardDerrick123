678e64487aea54fb15945244b9a1594a995e11e0
League Student <league@iMac-24.attlocal.net>
2020-03-04 18:29:05 -0800

    Crazy cat lady HiLoGame

--- src/_02_crazy_cat_lady/CrazyCatLady.java
+++ src/_02_crazy_cat_lady/CrazyCatLady.java
@@ -5,20 +5,25 @@ package _02_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+String Bird = JOptionPane.showInputDialog("How many Birds do you have");
 		// 2. Convert their answer into an int
-
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+if(Lady>2) {
+	JOptionPane.showInternalMessageDialog(null, "Your a Crazy Bird Lady");
+}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
-		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
+if(Lady<3 && Lady>0) {
+	playVideo("https://www.youtube.com/watch?v=8WaTd3z8sHE");
+}
+	//5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
+if(Lady==0) {
+	playVideo("https://www.youtube.com/watch?v=ZJT9CeEhM10");
 	}
-
 	static void playVideo(String videoURL) {
 		try {
 			URI uri = new URI(videoURL);
--- src/_03_high_low/HighLowGame.java
+++ src/_03_high_low/HighLowGame.java
@@ -12,11 +12,13 @@ public class HighLowGame {
 		int random = new Random().nextInt(5);
 		
 		// 2. Print out the random variable above
-		
+		System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
+		for (int i = 0; i < 10; i++) {
+			
 		
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
-
+Sting guess = JOPtionpane
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
 			
 			// 5. if the guess is correct
@@ -27,7 +29,7 @@ public class HighLowGame {
 			// 9. if the guess is low
 				// 10. Tell them it's too low
 
-		// 13. Tell them they lose
+		}// 13. Tell them they lose
 		
 	}
 
