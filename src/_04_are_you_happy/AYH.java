f715930ed68fcd084daef859e1c6c0c44b53e9b3
root <root@ed52009f6cd4>
2020-03-26 01:29:04 +0000

    level 0 mod 3

--- src/_04_are_you_happy/AYH.java
+++ src/_04_are_you_happy/AYH.java
@@ -1,5 +1,20 @@
 package _04_are_you_happy;
 
-public class AYH {
+import javax.swing.JOptionPane;
 
+public class AYH {
+public static void main(String[] args) {
+	String emotions= JOptionPane.showInputDialog("Are you happy");
+if (emotions.equals("yes")) {
+	JOptionPane.showMessageDialog(null, "Keep doing whatever your doing");
+
+}
+else {
+	JOptionPane.showInputDialog(null, "do you whant to be happy");
+if (emotions.contentEquals("yes")) {
+	JOptionPane.showMessageDialog(null, "Keep doing whatever your doing");
+
+}
+}
+}
 }
