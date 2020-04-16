54bbff5c6a28dca5db59b6acfb8f0379d8033b4c
root <root@3899ba0cdedc>
2020-04-16 01:24:14 +0000

    hireqa

--- src/_12_fizz_buzz/FizzBuzz.java
+++ src/_12_fizz_buzz/FizzBuzz.java
@@ -28,8 +28,20 @@ package _12_fizz_buzz;
 public class FizzBuzz {
 public static void main(String[] args) {
 	for (int i = 1;i<21  ; i++) {
-		System.out.println(i);
+		if(i%3==0 && i%5==0){
+		System.out.println("FuzzBizz");
+		}if (i%3==0) {
+			System.out.println("Fizz");}
+		else if (i%5==0) {
+				System.out.println("Buzz");
+		}
+		else {
+			System.out.println(i);
+			
+		}
+}
 	}
 }
-}
+
+
 
