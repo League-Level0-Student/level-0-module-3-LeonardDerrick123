698437aa91a5c065f645471043b8d04cf29241a8
root <root@9a8a52fcf5f0>
2020-05-20 23:56:02 +0000

    dizzy robot

--- src/_99_extra/DizzyRobot.java
+++ src/_99_extra/DizzyRobot.java
@@ -3,6 +3,8 @@
 
 package _99_extra;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class DizzyRobot {
@@ -10,14 +12,16 @@ public class DizzyRobot {
 	public static void main(String[] args) {
 		
 		 // 2. Ask the user how dizzy you want the robot from 1-10, then spin that number of times.
-
+JOptionPane.showInputDialog(null, "how dizzy dop you what the robot to feel from 1-10")
  // 1. Use the dance method to make the robot spin.
-	
+	if ( == "yes") {
+		
+	}()
 
 	}
 
 	static void dance(int numberOfSpins) {
-		for (int i = 0; i < numberOfSpins; i++) {
+		for (int i = 10; i < numberOfSpins; i++) {
 			robot.turn(360);
 		}
 	}
